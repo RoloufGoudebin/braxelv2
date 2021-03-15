b658a5aa9d37cbcaa4200fd8505d4327ea00b1e1
RoloufGoudebin <goudeloufrobin@gmail.com>
2021-03-15 23:57:20 +0100

    vive la rapidité

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { OmnicasaService } from './services/omnicasa/omnicasa.service';
+import { Property } from './services/omnicasa/interface';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'braxelv2';
+export class AppComponent{
+  title = 'braxel';
+
+  constructor(public omnicasa: OmnicasaService) { }
+
+
+  propertyList: Property[]; // liste des propriétés
+
+
+  ngOnInit() {
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ViewPropertyListComponent } from './view-property-list/view-property-list.component';
+import { OmnicasaService } from './services/omnicasa/omnicasa.service';
+import { ViewPropertyComponent } from './view-property/view-property.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ViewPropertyListComponent,
+    ViewPropertyComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [OmnicasaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
