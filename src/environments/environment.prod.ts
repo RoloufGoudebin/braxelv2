3c15a1d11ae3e6f07a6fd68b59df30de4f435beb
RoloufGoudebin <goudeloufrobin@gmail.com>
2021-03-17 15:05:40 +0100

    add environnement prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAZicRIOFgGaKWot_kmCCNaG5WyqVRnwTA",
+    authDomain: "braxel-map.firebaseapp.com",
+    projectId: "braxel-map",
+    storageBucket: "braxel-map.appspot.com",
+    messagingSenderId: "355805448566",
+    appId: "1:355805448566:web:9bf1bc90f09f448c371257",
+    measurementId: "G-B3ZY5FE140"
+  }
 };
