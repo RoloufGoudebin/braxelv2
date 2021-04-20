b8d3f5f5029ae820f8e4a4daf046863803408149
RoloufGoudebin <goudeloufrobin@gmail.com>
2021-04-20 18:29:50 +0200

    avis

--- src/app/avis/avis.component.ts
+++ src/app/avis/avis.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare const google: any;
 
 @Component({
   selector: 'app-avis',
   templateUrl: './avis.component.html',
   styleUrls: ['./avis.component.css']
 })
-export class AvisComponent implements OnInit {
+export class AvisComponent implements AfterViewInit {
+  service;
+  public reviews = [];
 
+  constructor() {}
 
-  constructor() { }
+  ngAfterViewInit() {
+    const request = {
+      placeId: "ChIJIxgLPcrRw0cREMNq7b82f-0",
+      fields: ['reviews']
+    };
+    this.service = new google.maps.places.PlacesService(document.getElementById('googleReviews'));
 
-  ngOnInit(): void {
+    this.service.getDetails(request, this.callback);
   }
 
+  public callback = (place, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      this.reviews = place.reviews.slice();
+    }
+  };
+
+  createRange(number) {
+    const items: number[] = [];
+    for (let i = 1; i <= number; i++) {
+      items.push(i);
+    }
+    return items;
+  }
 }
+
