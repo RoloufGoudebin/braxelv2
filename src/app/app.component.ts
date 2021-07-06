2de01e57baa4422806e5e274def4b55113c258a1
RoloufGoudebin <goudeloufrobin@gmail.com>
2021-07-06 11:31:39 +0200

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { OmnicasaService } from './services/omnicasa/omnicasa.service';
 import { Property } from './services/omnicasa/interface';
+import { RouterOutlet } from '@angular/router';
+import { routeTransitionAnimations } from './route-transition-animations';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [routeTransitionAnimations]
+
 })
 export class AppComponent{
   title = 'braxel';
@@ -20,6 +24,12 @@ export class AppComponent{
 
   ngOnInit() {
   }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && 
+      outlet.activatedRouteData && 
+      outlet.activatedRouteData['animationState'];
+   }
 
 
 }
