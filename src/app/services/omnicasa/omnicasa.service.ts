8cb95e63d7c37feec04da66e382ebc20d2577f70
Robin Goudelouf <goudeloufrobin@gmail.com>
2021-11-16 00:59:13 +0100

    version tempo

--- src/app/services/omnicasa/omnicasa.service.ts
+++ src/app/services/omnicasa/omnicasa.service.ts
@@ -11,34 +11,30 @@ export class OmnicasaService {
 
   constructor(public http: HttpClient) { }
 
-  link = "http://newapi.omnicasa.com/1.12/OmnicasaService.svc/GetPropertyListJson?json={%27DescriptionLanguageId%27:2,%20%27LanguageId%27:2,%20%27CustomerName%27:%27braxel%27,%20%27CustomerPassword%27:%27b688E6B8FDD2%27}";
+  link = "http://newapi.omnicasa.com/1.12/OmnicasaService.svc/GetPropertyListJson?json={%27LanguageId%27:2,%20%27CustomerName%27:%27braxel%27,%20%27CustomerPassword%27:%27b688E6B8FDD2%27}";
 
 
-  propertyList: PropertyList;
+  propertyList: Property[];
   property: Property;
+  toReturn: Observable<any>;
 
-  getPropertyList(): Observable<PropertyList> {
+  getPropertyList(): Observable<any> {
 
-    if (this.propertyList) {
-      console.log("reuse");
-      return of(this.propertyList);
-    }
-
-    return this.http.get<PropertyList>(this.link)
-      .pipe(tap((returnedData: PropertyList) => {
+    this.toReturn = this.http.get<PropertyList>(this.link)
+      .pipe(tap((returnedData: any) => {
         //save the returned data so we can re-use it later without making more HTTP calls
         this.propertyList = returnedData;
-        
       }));
+    return this.toReturn;
+
   }
 
 
-  getPropertyByID(id: number): Observable<Property>{
-    return this.http.get<Property>("http://newapi.omnicasa.com/1.12/OmnicasaService.svc/GetPropertiesByIDsJson?json={%27IDs%27:%27"+id+"%27,%20%27LanguageId%27:2,%20%27CustomerName%27:%27braxel%27,%20%27CustomerPassword%27:%27b688E6B8FDD2%27}")
+  getPropertyByID(id: number): Observable<Property> {
+    return this.http.get<Property>("http://newapi.omnicasa.com/1.12/OmnicasaService.svc/GetPropertiesByIDsJson?json={%27IDs%27:%27" + id + "%27,%20%27LanguageId%27:2,%20%27CustomerName%27:%27braxel%27,%20%27CustomerPassword%27:%27b688E6B8FDD2%27}")
       .pipe(tap((returnedData: Property) => {
         //save the returned data so we can re-use it later without making more HTTP calls
         this.property = returnedData;
-        
       }));
   }
 
