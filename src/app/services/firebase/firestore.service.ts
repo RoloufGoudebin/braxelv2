3464d0d2d27a80d4badf5167694ff348d11d6f7c
Rolouf Goudebin <robin@mamoot.be>
2021-12-13 23:14:22 +0100

    modifs graphiques

--- src/app/services/firebase/firestore.service.ts
+++ src/app/services/firebase/firestore.service.ts
@@ -87,7 +87,7 @@ export class FirestoreService {
         }
         return new Promise<Property>((resolve, reject) => {
           for (let i = 0, j = 0; i < this.propertyList.length; i++) {
-            if (invertPropertyList[i].SubStatus != 2 && invertPropertyList[i].ID > 200 && invertPropertyList[i].SubStatus != 3) {
+            if (invertPropertyList[i].SubStatus != 2 && invertPropertyList[i].SubStatus != 3) {
               this.firestore
                 .collection("sellProperties")
                 .doc(j.toString())
