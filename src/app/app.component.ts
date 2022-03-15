d572bc37eb685b4e179ae51d42947994eb4309c9
Rolouf Goudebin <robin@mamoot.be>
2022-03-15 10:44:41 +0100

    google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import{ Router, NavigationEnd, RouterOutlet} from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { routeTransitionAnimations } from './route-transition-animations';
 import { FirestoreService } from './services/firebase/firestore.service';
 
+
+
+declare let gtag: Function;
 
 
 @Component({
@@ -18,31 +21,42 @@ export class AppComponent {
   title = 'braxel';
   date;
 
-  constructor(private firestore: FirestoreService, private http: HttpClient, private translate: TranslateService) {
+  constructor(private firestore: FirestoreService, private http: HttpClient, private translate: TranslateService, private router: Router) {
     var lg = navigator.language;
     translate.setDefaultLang('fr');
-    translate.use(lg[0]+lg[1]);
+    translate.use(lg[0] + lg[1]);
     translate.addLangs(['en', 'fr', 'nl']);
-   }
+  }
 
   ngOnInit() {
-    this.firestore.updatePropertyListActive();
+    //this.firestore.updatePropertyListActive();
     //this.firestore.updatePropertyListSell()
     //this.firestore.createPropertyListSell();
     //this.firestore.createPropertyListActive();
     this.firestore.setPropertyListActiveFire();
-    this.firestore.getDateRefresh().subscribe(data=>
-      this.date= data.map(e => {
+    this.firestore.getDateRefresh().subscribe(data =>
+      this.date = data.map(e => {
         return {
           ...e.payload.doc.data() as any
         }
-    }));
+      }));
     setTimeout(() => {
-      if(Date.now() - this.date[0].lastRefresh > 3600000){
+      if (Date.now() - this.date[0].lastRefresh > 3600000) {
         this.firestore.updatePropertyListActive();
       }
     },
       3000);
+  }
+
+  setUpAnalytics() {
+    this.router.events
+      .subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-GNFNNFB0QP',
+          {
+            page_path: event.urlAfterRedirects
+          }
+        );
+      });
   }
 
   prepareRoute(outlet: RouterOutlet) {
@@ -61,6 +75,7 @@ export class AppComponent {
             ...e.payload.doc.data() as any
           }
         }));;
+
 
 
 }
